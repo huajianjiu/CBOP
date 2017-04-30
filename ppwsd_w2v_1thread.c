af5d56094655743d5add585d36e2bce90b4a9fee
huajianjiu <lastranger8811@gmail.com>
2017-05-01 02:25:36 +0900

    show training loss

--- ppwsd_w2v_1thread.c
+++ ppwsd_w2v_1thread.c
@@ -424,6 +424,8 @@ void TrainModelThread() {
   real *neu1 = (real *)calloc(layer1_size, sizeof(real));
   real *neu1e = (real *)calloc(layer1_size, sizeof(real));
   FILE *fi = fopen(train_file, "rb");
+  float sum_loss=0.0, loss_count=0.0;
+
   while (1) {
     if (word_count - last_word_count > 10000) {
       word_count_actual += word_count - last_word_count;
@@ -460,6 +462,10 @@ void TrainModelThread() {
     }
     if (feof(fi) || (word_count > train_words)) {
       word_count_actual += word_count - last_word_count;
+      // show loss
+      printf("loss: %f\n", (float)(sum_loss/(float)loss_count));
+      sum_loss = 0.0;
+      loss_count = 0.0;
       local_iter--;
       if (local_iter == 0) break;
       word_count = 0;
@@ -587,6 +593,14 @@ void TrainModelThread() {
             else g = (label - expTable[(int) ((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))]) * alpha;
             for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1neg[c + l2];
             for (c = 0; c < layer1_size; c++) syn1neg[c + l2] += g * neu1[c];
+            // sum loss
+            if (label == 0){
+              sum_loss += (float )g;
+              loss_count++;
+            } else {
+              sum_loss += ((float )1-g);
+              loss_count++;
+            }
           }
         // hidden -> in
         for (a = b; a < window * 2 + 1 - b; a++)
@@ -613,120 +627,6 @@ void TrainModelThread() {
   printf("free");
 }
 
-void *TestModelThread(void *id) {
-  long long a, b, d, cw, word, last_word, sentence_length = 0, sentence_position = 0;
-  long long word_count = 0, last_word_count = 0, sen[MAX_SENTENCE_LENGTH + 1];
-  long long l1, l2, c, target, label, local_iter = 1;
-  unsigned long long next_random = (long long)id;
-  real f, g;
-  clock_t now;
-  real *neu1 = (real *)calloc(layer1_size, sizeof(real));
-  real *neu1e = (real *)calloc(layer1_size, sizeof(real));
-  FILE *fi = fopen(train_file, "rb");
-  float sum_loss = 0;
-  long long loss_count = 0;
-
-  fseek(fi, file_size / (long long)num_threads * (long long)id, SEEK_SET);
-  while (1) {
-    if (word_count - last_word_count > 10000) {
-      word_count_actual += word_count - last_word_count;
-      last_word_count = word_count;
-      if ((debug_mode > 1)) {
-        now=clock();
-        printf("%cAlpha: %f  Progress: %.2f%%  Words/thread/sec: %.2fk  ", 13, alpha,
-               word_count_actual / (real)(1 * train_words + 1) * 100,
-               word_count_actual / ((real)(now - start + 1) / (real)CLOCKS_PER_SEC * 1000));
-        fflush(stdout);
-      }
-      alpha = starting_alpha * (1 - word_count_actual / (real)(1 * train_words + 1));
-      if (alpha < starting_alpha * 0.0001) alpha = starting_alpha * 0.0001;
-    }
-    if (sentence_length == 0) {
-      while (1) {
-        word = ReadWordIndex(fi);
-        if (feof(fi)) break;
-        if (word == -1) continue;
-        word_count++;
-        if (word == 0) break;
-        // The subsampling randomly discards frequent words while keeping the ranking same
-        if (sample > 0) {
-          real ran = (sqrt(vocab[word].cn / (sample * train_words)) + 1) * (sample * train_words) / vocab[word].cn;
-          next_random = next_random * (unsigned long long)25214903917 + 11;
-          if (ran < (next_random & 0xFFFF) / (real)65536) continue;
-        }
-        sen[sentence_length] = word;
-        sentence_length++;
-        if (sentence_length >= MAX_SENTENCE_LENGTH) break;
-      }
-      sentence_position = 0;
-    }
-    if (feof(fi) || (word_count > train_words / num_threads)) {
-      word_count_actual += word_count - last_word_count;
-      local_iter--;
-      if (local_iter == 0) break;
-      word_count = 0;
-      last_word_count = 0;
-      sentence_length = 0;
-      fseek(fi, file_size / (long long)num_threads * (long long)id, SEEK_SET);
-      continue;
-    }
-    word = sen[sentence_position];
-    if (word == -1) continue;
-    for (c = 0; c < layer1_size; c++) neu1[c] = 0;
-    for (c = 0; c < layer1_size; c++) neu1e[c] = 0;
-    next_random = next_random * (unsigned long long)25214903917 + 11;
-    b = next_random % window;
-    if (cbow) {  //train the cbow architecture
-      // in -> hidden
-      cw = 0;
-      for (a = b; a < window * 2 + 1 - b; a++) if (a != window) {
-          c = sentence_position - window + a;
-          if (c < 0) continue;
-          if (c >= sentence_length) continue;
-          last_word = sen[c];
-          if (last_word == -1) continue;
-          for (c = 0; c < layer1_size; c++) neu1[c] += syn0[c + last_word * layer1_size];
-          cw++;
-        }
-      if (cw) {
-        for (c = 0; c < layer1_size; c++) neu1[c] /= cw;
-        // NEGATIVE SAMPLING
-        if (negative > 0) for (d = 0; d < negative + 1; d++) {
-            if (d == 0) {
-              target = word;
-              label = 1;
-            } else {
-              next_random = next_random * (unsigned long long)25214903917 + 11;
-              target = table[(next_random >> 16) % table_size];
-              if (target == 0) target = next_random % (vocab_size - 1) + 1;
-              if (target == word) continue;
-              label = 0;
-            }
-            l2 = target * layer1_size;
-            f = 0;
-            for (c = 0; c < layer1_size; c++) f += neu1[c] * syn1neg[c + l2];
-            if (f > MAX_EXP) g = (label - 1) * alpha;
-            else if (f < -MAX_EXP) g = (label - 0) * alpha;
-            else g = (label - expTable[(int)((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))]) * alpha;
-            if (label == 1) sum_loss += 1-g;
-            else if (label == 0) sum_loss += g;
-            loss_count++;
-          }
-      }
-    }
-    sentence_position++;
-    if (sentence_position >= sentence_length) {
-      sentence_length = 0;
-      continue;
-    }
-  }
-  printf("loss: %f\n", (sum_loss/(float)loss_count));
-  fclose(fi);
-  free(neu1);
-  free(neu1e);
-  pthread_exit(NULL);
-}
-
 void TrainModel() {
   long a, b;
   FILE *fo, *fot;
@@ -742,9 +642,6 @@ void TrainModel() {
   if (negative > 0) InitUnigramTable();
   start = clock();
   TrainModelThread();
-  //show training loss
-  for (a = 0; a < 1; a++) pthread_create(&pt[a], NULL, TestModelThread, (void *)a);
-  for (a = 0; a < 1; a++) pthread_join(pt[a], NULL);
 //  printf("here?\n");
   fo = fopen(strcat(output_file, ".bin"), "wb");
   fot = fopen(strcat(output_file, ".txt"), "wb");
