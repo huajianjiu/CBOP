53685b364dc16003d1be5b6127b7b646c8f968ad
huajianjiu <lastranger8811@gmail.com>
2017-05-23 19:50:59 +0900

    update

--- EwcWord2vec.py
+++ EwcWord2vec.py
@@ -1,9 +1,8 @@
 from gensim.models.word2vec import *
 
 # TODO: check whether the ewc train_sg_pair and train_cbow_pair is used.
-# TODO: if no, override the cython functions in the gensim implementation
 
-# Override the train_batch_sg, train_batch_cbow, to make them use the train_xx_pair in this module.
+# Override the train_batch_sg, train_batch_cbow, to make it use the train_xx_pair functions defined here.
 # Plain Python. Cython ones in the future.
 
 FAST_VERSION = -1
@@ -182,24 +181,51 @@ def train_cbow_pair(model, word, input_word_indices, l1, alpha, learn_vectors=Tr
     return neu1e
 
 
-def fisher_batch_sg(model, sentences, alpha, work=None):
+def fisher_batch_sg(model, sentences, work=None):
+    # accumulate fisher information
+    # remember to report accumulate times
+    word_count = 0
+    accum_count = 0
+    for sentence in sentences:
+        word_vocabs = [model.wv.vocab[w] for w in sentence if w in model.wv.vocab and
+                       model.wv.vocab[w].sample_int > model.random.rand() * 2 ** 32]
+        for pos, word in enumerate(word_vocabs):
+            reduced_window = model.random.randint(model.window)  # `b` in the original word2vec code
+
+            # now go over all words from the (reduced) window, predicting each one in turn
+            start = max(0, pos - model.window + reduced_window)
+            for pos2, word2 in enumerate(word_vocabs[start:(pos + model.window + 1 - reduced_window)], start):
+                # don't train on the `word` itself
+                if pos2 != pos:
+                    accum_count += fisher_sg_pair(model, model.wv.index2word[word.index], word2.index)
+        word_count += len(word_vocabs)
+    return word_count, accum_count
+
+def fisher_batch_cbow(model, sentences, work=None, neu1=None):
+    # TODO: accumulate fisher information
     # TODO: remember to average the fisher information
-    pass
+    # TODO: remember to report accumulate times
+    word_count = 0
+    accum_count = 0
+
+    return word_count, accum_count
+
+def fisher_sg_pair(model, word, context_index, learn_vectors=True, learn_hidden=True,
+                   context_vectors=None, context_locks=None):
+    # TODO: accumulate fisher information
+    # TODO: remember to report accumulate times
+    accum_count = 0
+
+    return accum_count
 
 
-def fisher_batch_cbow(model, sentences, alpha, work=None, neu1=None):
-    # TODO: remember to average the fisher information
-    pass
-
-
-def fisher_sg_pair(model, word, context_index, alpha, learn_vectors=True, learn_hidden=True,
-                  context_vectors=None, context_locks=None):
-    pass
-
-
-def fisher_cbow_pair(model, word, input_word_indices, l1, alpha, learn_vectors=True, learn_hidden=True):
+def fisher_cbow_pair(model, word, input_word_indices, l1, learn_vectors=True, learn_hidden=True):
     neu1e = zeros(l1.shape)
-    pass
+    # TODO: accumulate fisher information
+    # TODO: remember to report accumulate times
+    accum_count = 0
+
+    return accum_count
 
 
 class EwcWord2vec(Word2Vec):
@@ -252,25 +278,161 @@ class EwcWord2vec(Word2Vec):
             self.star_syn1 = self.syn1
         if hasattr(self, "syn1neg"):
             self.star_syn1neg = self.syn1neg
-        self.calculate_fisher(sentences)
+        self.calculate_fisher(sentences, total_examples=total_examples, total_words=total_words)
 
         return trained_word_count
 
-    def calculate_fisher(self, sentences):
-        # TODO: calculate fisher information using the frequent words.
-        # TODO: for example, if power is 0.75, the corpus size is 1000, the most 750 frequent words are used.
-        # TODO: sample for one_class(A word) or for all words
-        # TODO: when sample one class, use the distribution the same as the the ns, i.e. w.r.t. tf*power
+    def calculate_fisher(self, sentences, total_examples=None, total_words=None,
+                         word_count=0,
+                         queue_factor=1, report_delay=1.0):
+        # calculate fisher information using all of the samples to max cover the embedding layer and hidden layer.
         # the vocab is sorted if this is no revision of the gensim implementation. May 2017.
         # to cover the whole parameters, similar with training, accumulate fisher information
         # Notice: use the all samples to cover the whole embedding layer as more as possible
 
         job_tally = 0
-        # TODO: worker loop. accumulate the squared gradient of log likelihood
 
-        # TODO: divide the accmulated squared log likelihood with the total
+        print("Start to accumulate Fisher information.")
 
-        pass
+        logger.info("Start to accumulate Fisher information.")
+
+        # worker loop(consumer) and job producer. accumulate the squared gradient of log likelihood
+        def worker_loop():
+            """Accumulate Fisher Information, lifting lists of sentences from the job_queue."""
+            work = matutils.zeros_aligned(self.layer1_size, dtype=REAL)  # per-thread private work memory
+            neu1 = matutils.zeros_aligned(self.layer1_size, dtype=REAL)
+            jobs_processed = 0
+            while True:
+                job = job_queue.get()
+                if job is None:
+                    progress_queue.put(None)
+                    break  # no more jobs => quit this worker
+                sentences = job
+                accum, tally, raw_tally = self._do_fisher_job(sentences, (work, neu1))
+                progress_queue.put((len(sentences), accum, tally, raw_tally))  # report back progress and accum_count
+                jobs_processed += 1
+            logger.debug("worker exiting, processed %i jobs", jobs_processed)
+
+        def job_producer():
+            """Fill jobs queue using the input `sentences` iterator."""
+            job_batch, batch_size = [], 0
+            pushed_words, pushed_examples = 0, 0
+            job_no = 0
+
+            for sent_idx, sentence in enumerate(sentences):
+                sentence_length = self._raw_word_count([sentence])
+
+                # can we fit this sentence into the existing job batch?
+                if batch_size + sentence_length <= self.batch_words:
+                    # yes => add it to the current job
+                    job_batch.append(sentence)
+                    batch_size += sentence_length
+                else:
+                    # no => submit the existing job
+                    logger.debug(
+                        "queueing job #%i (%i words, %i sentences)",
+                        job_no, batch_size, len(job_batch))
+                    job_no += 1
+                    job_queue.put(job_batch)
+
+                    # add the sentence that didn't fit as the first item of a new job
+                    job_batch, batch_size = [sentence], sentence_length
+
+            # add the last job too (may be significantly smaller than batch_words)
+            if job_batch:
+                logger.debug(
+                    "queueing job #%i (%i words, %i sentences)",
+                    job_no, batch_size, len(job_batch))
+                job_no += 1
+                job_queue.put(job_batch)
+
+            if job_no == 0 and self.train_count == 0:
+                logger.warning(
+                    "train() called with an empty iterator (if not intended, "
+                    "be sure to provide a corpus that offers restartable "
+                    "iteration = an iterable)."
+                )
+
+            # give the workers heads up that they can finish -- no more work!
+            for _ in xrange(self.workers):
+                job_queue.put(None)
+            logger.debug("job loop exiting, total %i jobs", job_no)
+
+        # buffer ahead only a limited number of jobs.. this is the reason we can't simply use ThreadPool :(
+        job_queue = Queue(maxsize=queue_factor * self.workers)
+        progress_queue = Queue(maxsize=(queue_factor + 3) * self.workers)
+
+        workers = [threading.Thread(target=worker_loop) for _ in xrange(self.workers)]
+        unfinished_worker_count = len(workers)
+        workers.append(threading.Thread(target=job_producer))
+
+        for thread in workers:
+            thread.daemon = True  # make interrupting the process with ctrl+c easier
+            thread.start()
+
+        example_count, accum_count, trained_word_count, raw_word_count = 0, 0, word_count
+        start, next_report = default_timer() - 0.00001, 1.0
+
+        while unfinished_worker_count > 0:
+            report = progress_queue.get()  # blocks if workers too slow
+            if report is None:  # a thread reporting that it finished
+                unfinished_worker_count -= 1
+                logger.info("worker thread finished; awaiting finish of %i more threads", unfinished_worker_count)
+                continue
+            examples, accums, trained_words, raw_words = report
+            job_tally += 1
+
+            # update progress stats
+            example_count += examples
+            accum_count += accums
+            trained_word_count += trained_words  # only words in vocab & sampled
+            raw_word_count += raw_words
+
+            # log progress once every report_delay seconds
+            elapsed = default_timer() - start
+            if elapsed >= next_report:
+                if total_examples:
+                    # examples-based progress %
+                    logger.info(
+                        "PROGRESS: at %.2f%% examples, %.0f words/s, in_qsize %i, out_qsize %i",
+                        100.0 * example_count / total_examples, trained_word_count / elapsed,
+                        utils.qsize(job_queue), utils.qsize(progress_queue))
+                else:
+                    # words-based progress %
+                    logger.info(
+                        "PROGRESS: at %.2f%% words, %.0f words/s, in_qsize %i, out_qsize %i",
+                        100.0 * raw_word_count / total_words, trained_word_count / elapsed,
+                        utils.qsize(job_queue), utils.qsize(progress_queue))
+                next_report = elapsed + report_delay
+
+        # all done; report the final stats
+        elapsed = default_timer() - start
+        logger.info(
+            "Accumulate Fisher Information on %i raw words (%i effective words) took %.1fs, %.0f effective words/s",
+            raw_word_count, trained_word_count, elapsed, trained_word_count / elapsed)
+        if job_tally < 10 * self.workers:
+            logger.warning(
+                "under 10 jobs per worker: consider setting a smaller `batch_words' for smoother alpha decay"
+            )
+
+        # check that the input corpus hasn't changed during iteration
+        if total_examples and total_examples != example_count:
+            logger.warning(
+                "supplied example count (%i) did not equal expected count (%i)", example_count, total_examples
+            )
+        if total_words and total_words != raw_word_count:
+            logger.warning(
+                "supplied raw word count (%i) did not equal expected count (%i)", raw_word_count, total_words
+            )
+
+        self.clear_sims()
+
+        # divide the accmulated squared log likelihood with accum_count
+        self.fisher_syn0 /= accum_count
+        self.fisher_syn1 /= accum_count
+        self.fisher_syn1neg /= accum_count
+
+        return accum_count
 
     def _do_train_job(self, sentences, alpha, inits):
         # override it by a clone to use the train_batch_xx in this module
@@ -287,12 +449,17 @@ class EwcWord2vec(Word2Vec):
             tally += train_batch_cbow(self, sentences, alpha, work, neu1)
         return tally, self._raw_word_count(sentences)
 
-    def _do_fisher_job(self, sentences, alpha, inits):
+    def _do_fisher_job(self, sentences, inits):
         print("ewc do_fisher_job")
         work, neu1 = inits
         tally = 0
+        accum = 0
         if self.sg:
-            tally += fisher_batch_sg(self, sentences, alpha, work)
+            word_count, accum_count = fisher_batch_sg(self, sentences, work)
+            tally += word_count
+            accum += accum_count
         else:
-            tally += fisher_batch_cbow(self, sentences, alpha, work, neu1)
-        return tally, self._raw_word_count(sentences)
+            word_count, accum_count = fisher_batch_cbow(self, sentences, work, neu1)
+            tally += word_count
+            accum += accum_count
+        return accum, tally, self._raw_word_count(sentences)
