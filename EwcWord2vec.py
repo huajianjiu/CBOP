bbce6a2b77437b3de5e26e436c2609aa80ee302c
huajianjiu <lastranger8811@gmail.com>
2017-05-23 17:35:30 +0900

    dev

--- EwcWord2vec.py
+++ EwcWord2vec.py
@@ -182,10 +182,32 @@ def train_cbow_pair(model, word, input_word_indices, l1, alpha, learn_vectors=Tr
     return neu1e
 
 
+def fisher_batch_sg(model, sentences, alpha, work=None):
+    # TODO: remember to average the fisher information
+    pass
+
+
+def fisher_batch_cbow(model, sentences, alpha, work=None, neu1=None):
+    # TODO: remember to average the fisher information
+    pass
+
+
+def fisher_sg_pair(model, word, context_index, alpha, learn_vectors=True, learn_hidden=True,
+                  context_vectors=None, context_locks=None):
+    pass
+
+
+def fisher_cbow_pair(model, word, input_word_indices, l1, alpha, learn_vectors=True, learn_hidden=True):
+    neu1e = zeros(l1.shape)
+    pass
+
+
 class EwcWord2vec(Word2Vec):
     def __init__(self, sentences=None, size=100, alpha=0.025, window=5, min_count=5, max_vocab_size=None, sample=1e-3,
                  seed=1, workers=3, min_alpha=0.0001, sg=0, hs=0, negative=5, cbow_mean=1, hashfxn=hash, iter=5,
-                 null_word=0, trim_rule=None, sorted_vocab=1, batch_words=MAX_WORDS_IN_BATCH, lam=15):
+                 null_word=0, trim_rule=None, sorted_vocab=1, batch_words=MAX_WORDS_IN_BATCH, lam=15,
+                 vocabfile="fullwiki_vocab.txt"):
+        self.vocabfile = vocabfile
         self.lam = lam
         self.fisher_syn0 = None
         self.fisher_syn1 = None
@@ -197,6 +219,17 @@ class EwcWord2vec(Word2Vec):
                                           workers, min_alpha, sg, hs, negative, cbow_mean, hashfxn, iter, null_word,
                                           trim_rule, sorted_vocab, batch_words)
 
+    def scan_vocab(self, sentences, progress_per=10000, trim_rule=None):
+        # need the sentence count so call the original one
+        super(EwcWord2vec, self).scan_vocab(sentences=sentences, progress_per=progress_per, trim_rule=trim_rule)
+        # scan external vocab and update raw_vocab
+        logger.info("collecting all words and their counts from external vocab file")
+        vocab = self.raw_vocab
+        with open(self.vocabfile, "r") as f:
+            for line in f.readlines:
+                vocab[line.split(" ")[0]] = line.split(" ")[1]
+        self.raw_vocab = vocab
+
     def train(self, sentences, total_examples=None, total_words=None,
               epochs=None, start_alpha=None, end_alpha=None,
               word_count=0,
@@ -204,7 +237,7 @@ class EwcWord2vec(Word2Vec):
         # The train override the train of Word2Vec and will be called in the super().__init__
         # print ("train!")
         # print (hasattr(self, "syn1"))
-        # TODO: get fisher information at the end
+        # get fisher information at the end
         # hope the overrided _do_train_job will be used
         trained_word_count = \
             super(EwcWord2vec, self).train(sentences, total_examples=total_examples, total_words=total_words,
@@ -219,37 +252,23 @@ class EwcWord2vec(Word2Vec):
             self.star_syn1 = self.syn1
         if hasattr(self, "syn1neg"):
             self.star_syn1neg = self.syn1neg
-        self.calculate_fisher(sentences, 0.75)
+        self.calculate_fisher(sentences)
 
         return trained_word_count
 
-    def calculate_fisher(self, sentences, class_num=1, sample_num=200):
+    def calculate_fisher(self, sentences):
         # TODO: calculate fisher information using the frequent words.
         # TODO: for example, if power is 0.75, the corpus size is 1000, the most 750 frequent words are used.
         # TODO: sample for one_class(A word) or for all words
         # TODO: when sample one class, use the distribution the same as the the ns, i.e. w.r.t. tf*power
         # the vocab is sorted if this is no revision of the gensim implementation. May 2017.
-        # For hs:
-        # 1. randomly select a class(a word)
-        # 2. get a sample context from the corpus
-        # 3. get log likelihood of the probability of the code of the selected class (word) given the sample context
-        #    (the sum of the log likelihood of each code number)
-        # 4. get the squared gradient vector in the directions of each parameter(every embedding, every theta)
-        # 5. repeat 2-4 until samples number reach sample_num
-        # 6. average the squared gradient vector in the directions of each parameter
-        # For ns:
-        # 1. randomly select zero or one
-        # 2. get a sample context from the corpus
-        # 3. get a random word in the sample context
-        # 4. get the squared gradient vector in the directions of each parameter(every embedding, every theta)
-        # 5. repeat 2-4 until samples number reach sample_num
-        # 6. average the squared gradient vector in the directions of each parameter
+        # to cover the whole parameters, similar with training, accumulate fisher information
+        # Notice: use the all samples to cover the whole embedding layer as more as possible
 
-        if class_num > 1:
-            logger.warning("sample fisher information for multiple classes is uncompleted.")
-            return 0
+        job_tally = 0
+        # TODO: worker loop. accumulate the squared gradient of log likelihood
 
-        w_ind = self.cum_table.searchsorted(self.random.randint(self.cum_table[-1]))
+        # TODO: divide the accmulated squared log likelihood with the total
 
         pass
 
@@ -266,4 +285,14 @@ class EwcWord2vec(Word2Vec):
             tally += train_batch_sg(self, sentences, alpha, work)
         else:
             tally += train_batch_cbow(self, sentences, alpha, work, neu1)
+        return tally, self._raw_word_count(sentences)
+
+    def _do_fisher_job(self, sentences, alpha, inits):
+        print("ewc do_fisher_job")
+        work, neu1 = inits
+        tally = 0
+        if self.sg:
+            tally += fisher_batch_sg(self, sentences, alpha, work)
+        else:
+            tally += fisher_batch_cbow(self, sentences, alpha, work, neu1)
         return tally, self._raw_word_count(sentences)
