abc4a5230379d76b942a02f6de2aa7500b4b7516
huajianjiu <lastranger8811@gmail.com>
2017-05-24 15:59:35 +0900

    update

--- testEwcWord2vec.py
+++ testEwcWord2vec.py
@@ -1,7 +1,7 @@
 from EwcWord2vec import *
 
-brown = BrownCorpus()
 text8 = Text8Corpus("../Corpus/text8")
+ukwac = Text8Corpus("../ukWac/ukwac_subset_10M_processed")
 
-model = EwcWord2vec(brown)
-model.train(text8)
+model = EwcWord2vec(text8)
+model.train(ukwac)
